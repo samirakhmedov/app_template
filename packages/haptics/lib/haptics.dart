05088083b91f8c76b62d868c6e6ac459fae560ba
Samir Akhmedov <lutrak.developer@gmail.com>
2025-08-28 00:24:13 +0400

    fix: removed local haptic implementation with remote one

--- packages/haptics/lib/haptics.dart
+++
@@ -1,4 +0,0 @@
-export 'src/entities/haptic_feedback.dart' show HapticFeedbackType;
-export 'src/entities/haptic_impact.dart' show HapticImpactType;
-export 'src/entities/haptic_notification.dart' show HapticNotificationType;
-export 'src/haptics.dart' show Haptics;
